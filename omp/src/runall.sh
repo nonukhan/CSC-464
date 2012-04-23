8d5afbcb0de6242ae6c27219e5ff87c24bf03a20
Jeremy Ho <jujaga@gmail.com>
2012-04-23 02:11:05 -0700

    Tweaked runall script to do 100 & rcateg 1 & 8
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- omp/src/runall.sh
+++ omp/src/runall.sh
@@ -1,16 +1,27 @@
 #! /bin/bash
 
-iters=1000
+iters=100
 inpath="../testdata/"
 outpath="../analysis/"
 files="114bp-5sp-test.phy 60bp-30sp-test.phy 300bp-5sp-test.phy 300bp-30sp-test.phy"
 
 for f in $files; do
 
-	outfile=`echo $f | cut -f1 -d .`"-all.txt"
+	outfile=`echo $f | cut -f1 -d .`"-all-1.txt"
 	for (( i=0; i<iters; i++ )); do
-		./dnaml $inpath$f 2>> $outpath$outfile > /dev/null
+		./dnaml1 $inpath$f 2>> $outpath$outfile > /dev/null
 
 	done
 
 done
+
+for f in $files; do
+
+	outfile=`echo $f | cut -f1 -d .`"-all-8.txt"
+	for (( i=0; i<iters; i++ )); do
+		./dnaml8 $inpath$f 2>> $outpath$outfile > /dev/null
+
+	done
+
+done
+
